4f02f5349ec005e21d24f3db7b481d3dca379981
kiki_doYou <kiki_1881@yandex.ru>
2022-05-04 13:56:39 +0300

    elba ground done

--- js/scripts.js
+++ js/scripts.js
@@ -64,3 +64,70 @@ modalClose2.addEventListener('click', function () {
   modalBg2.classList.remove('bg-active');
 });
 //
+
+const tl = gsap.timeline({ defaults: { duration: 0.5 } });
+
+tl.from('.menu', { y: -100 })
+  .from('.logo', { opacity: 0 })
+  .from('.content__title', { opacity: 0, delay: 0.5, y: 30 })
+  .from('.content__subtitle', { opacity: 0, stagger: 1, y: 60 });
+
+const animBreakpoint = 600;
+if (window.scrollY >= animBreakpoint) {
+  tl.from('.projects', { opacity: 0 });
+}
+
+const scrollItemLink = document.querySelectorAll('.projects__item-link');
+const scrollAbout = document.querySelectorAll('.about');
+const scrollFooter = document.querySelectorAll('footer');
+
+const scrollAnimation = () => {
+  let windowCenter = window.innerHeight / 2 + window.scrollY;
+  scrollItemLink.forEach((el) => {
+    let scrollOffset = el.offsetTop + el.offsetHeight * 2.6;
+    if (windowCenter >= scrollOffset) {
+      el.classList.add('animation__class');
+    } else {
+      el.classList.remove('animation__class');
+    }
+  });
+};
+
+const scrollAboutAnimation = () => {
+  let windowCenter = window.innerHeight / 2 + window.scrollY;
+  scrollAbout.forEach((el) => {
+    let scrollOffset = el.offsetTop + el.offsetHeight * 15;
+    if (windowCenter >= scrollOffset) {
+      el.classList.add('animation__class');
+    } else {
+      el.classList.remove('animation__class');
+    }
+  });
+};
+
+const scrollFooterAnimation = () => {
+  let windowCenter = window.innerHeight / 2 + window.scrollY;
+  scrollFooter.forEach((el) => {
+    let scrollOffset = el.offsetTop + el.offsetHeight / 40;
+    if (windowCenter >= scrollOffset) {
+      el.classList.add('animation__class');
+    } else {
+      el.classList.remove('animation__class');
+    }
+  });
+};
+
+scrollAnimation();
+scrollAboutAnimation();
+scrollFooterAnimation();
+window.addEventListener('scroll', () => {
+  scrollAnimation();
+  scrollAboutAnimation();
+  scrollFooterAnimation();
+});
+
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 500,
+  speedAsDuration: true,
+});
+
